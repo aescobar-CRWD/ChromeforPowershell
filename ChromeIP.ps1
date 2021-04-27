b0d4dc3df0648989244c5f6b7207ea8a1f6aa28e
aescobar-CRWD <53222444+aescobar-CRWD@users.noreply.github.com>
2021-04-27 22:32:05 +0100

    Update ChromeIP.ps1

--- ChromeIP.ps1
+++ ChromeIP.ps1
@@ -1,6 +1,6 @@
-Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n172.17.0.21`twww.1password.com" -Force 
+Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n172.17.0.21`t1password.com" -Force 
 $pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-$url1 = "http://www.1password.com:8080"
+$url1 = "http://1password.com:8080"
 #$url2 = "https://www.salesforce.com/"
 
 Start-Process -FilePath $pathToChrome -ArgumentList $url1, --no-sandbox
