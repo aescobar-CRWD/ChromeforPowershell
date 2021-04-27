f0fe868e75bf1b642d304a7a973f18327c68f672
aescobar-CRWD <53222444+aescobar-CRWD@users.noreply.github.com>
2021-04-27 22:20:19 +0100

    Update ChromeIP.ps1
    Commenting site 2
    Modifying URL

--- ChromeIP.ps1
+++ ChromeIP.ps1
@@ -1,7 +1,7 @@
 $pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 $url1 = "http://www.1password.com:8080"
-$url2 = "https://www.salesforce.com/"
+#$url2 = "https://www.salesforce.com/"
 
 Start-Process -FilePath $pathToChrome -ArgumentList $url1, --no-sandbox
-Start-Sleep -s 2
-Start-Process -FilePath $pathToChrome -ArgumentList $url2, --no-sandbox
+#Start-Sleep -s 2
+#Start-Process -FilePath $pathToChrome -ArgumentList $url2, --no-sandbox
